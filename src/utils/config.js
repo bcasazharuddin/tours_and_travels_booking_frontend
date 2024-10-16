074da20a805a5389fb49d8d4dd60a2a75a06662f
azharuddin <azharuddin.intern@faircent.com>
2024-10-16 19:25:12 +0530

    add config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 
 
-export const BASE_URL = 'http://localhost:4000/api/v1'
+// export const BASE_URL = 'http://localhost:4000/api/v1'
 
-// export const BASE_URL = 'https://tours-booking-backend-9bz7.onrender.com/api/v1'
+export const BASE_URL = 'https://tours-booking-backend-9bz7.onrender.com/api/v1'
